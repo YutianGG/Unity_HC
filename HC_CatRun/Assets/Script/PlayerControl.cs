b2eccc7719177ad2b4cdda21e793fb899246859d
Yushian <73380147+YutianGG@users.noreply.github.com>
2022-11-12 21:25:17 +0800

    20221112
    人物渲染

--- HC_CatRun/Assets/Script/PlayerControl.cs
+++ HC_CatRun/Assets/Script/PlayerControl.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] Rigidbody RB;
+    [SerializeField] Animator ani;
     [SerializeField] float Speed = 2f;
     [SerializeField] float MouseSpeed = 1.5f;
     [SerializeField] float JumpSpeed = 50f;
     [SerializeField] Transform Camera;
-
+    [SerializeField] Transform PlayerY;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -24,25 +25,41 @@ public class PlayerControl : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
         //Debug.Log(vertical + " ," + horizontal);
+        ani.SetFloat("Vertical", vertical);
+        ani.SetFloat("Horizontal", horizontal);
 
         //設定座標
         Vector3 Move = new Vector3
-            (vertical * Speed, RB.velocity.y, -horizontal * Speed);
+            (horizontal * Speed, RB.velocity.y, vertical * Speed);
+        
+        //修正座標
+        Move = PlayerY.transform.TransformDirection(Move);
 
-        Move = this.transform.TransformDirection(Move);//修正座標
-        RB.velocity = Move;//修改RB速率為向量Move
+        //修改RB速率為向量Move
+        RB.velocity = Move;
 
-        float mouseX = Input.GetAxis("Mouse X");//取得滑鼠水平位移  
-        //this.transform.Rotate(0, mouseX * MouseSpeed, 0);
-        RB.angularVelocity = new Vector3(0f, mouseX * MouseSpeed, 0f);//旋轉物件
+        //取得滑鼠水平位移  
+        float mouseX = Input.GetAxis("Mouse X");
 
-        mouseY += Input.GetAxis("Mouse Y") * MouseSpeed;//取得滑鼠Y座標移動
-        mouseY = Mathf.Clamp(mouseY, -80f, 80f);//限制數值於-80到80之間
-        Quaternion rotation = Quaternion.Euler(mouseY * -1f, 90f, 0f);//建立新旋轉值
-        Camera.transform.localRotation = rotation;//旋轉攝影機
+        //旋轉物件
+        PlayerY.transform.Rotate(0, mouseX * MouseSpeed, 0);
+        //RB.angularVelocity = new Vector3(0f, mouseX * MouseSpeed, 0f);
 
-        if (Input.GetKeyDown(KeyCode.Space) && onFlood)//跳躍
-            RB.velocity = new Vector3(0f, JumpSpeed, 0f); 
+        //取得滑鼠Y座標移動
+        mouseY += Input.GetAxis("Mouse Y") * MouseSpeed;
+
+        //限制數值於-80到80之間
+        mouseY = Mathf.Clamp(mouseY, -80f, 80f);
+
+        //建立新旋轉值
+        Quaternion rotation = Quaternion.Euler(mouseY * -1f, 0f, 0f);
+
+        //旋轉攝影機
+        Camera.transform.localRotation = rotation;
+
+        //跳躍
+        if (Input.GetKeyDown(KeyCode.Space) && onFlood)
+            RB.velocity = new Vector3(RB.velocity.x, JumpSpeed, RB.velocity.z); 
     }
 
     bool onFlood = false;//跳躍判定
