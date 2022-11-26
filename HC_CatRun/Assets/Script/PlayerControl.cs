c7a2755f7a17c6f9b8d76c412f4bd8cef3682967
Yushian <73380147+YutianGG@users.noreply.github.com>
2022-11-26 21:21:51 +0800

    20221126

--- HC_CatRun/Assets/Script/PlayerControl.cs
+++ HC_CatRun/Assets/Script/PlayerControl.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -12,19 +13,33 @@ public class PlayerControl : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Energy = maxEnergy;
     }
     private void Update()
     {
         Move();
+        Dodge();
     }
-
+    float vertical = 0f;
+    float horizontal = 0f;
     float mouseY = 0f;//滑鼠位移
     private void Move()
     {
         //取得方向鍵
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        //Debug.Log(vertical + " ," + horizontal);
+        float local_vertical = Input.GetAxisRaw("Vertical");
+        float local_horizontal = Input.GetAxisRaw("Horizontal");
+        //Debug.Log(vertical + " ," + horizontal);       
+
+        vertical = Mathf.Lerp(vertical, local_vertical, Time.deltaTime * 10f);
+        horizontal = Mathf.Lerp(horizontal, local_horizontal, Time.deltaTime * 10f);
+
+        float FV = ani.GetFloat("FV");
+        float FH = ani.GetFloat("FH");
+        float T = ani.GetFloat("T");
+        vertical = Mathf.Lerp(vertical, FV, T);
+        horizontal = Mathf.Lerp(horizontal, FH, T);
+
+        //設定動畫參數
         ani.SetFloat("Vertical", vertical);
         ani.SetFloat("Horizontal", horizontal);
 
@@ -37,7 +52,7 @@ public class PlayerControl : MonoBehaviour
 
         //修改RB速率為向量Move
         RB.velocity = Move;
-
+       
         //取得滑鼠水平位移  
         float mouseX = Input.GetAxis("Mouse X");
 
@@ -58,14 +73,93 @@ public class PlayerControl : MonoBehaviour
         Camera.transform.localRotation = rotation;
 
         //跳躍
-        if (Input.GetKeyDown(KeyCode.Space) && onFlood)
-            RB.velocity = new Vector3(RB.velocity.x, JumpSpeed, RB.velocity.z); 
+        if (Input.GetKeyDown(KeyCode.F) && onFloor)
+            RB.velocity = new Vector3(RB.velocity.x, JumpSpeed, RB.velocity.z);
+    }
+    private void Dodge()
+    {   
+        //每一秒加上10體力
+        Energy += Time.deltaTime*10f;
+
+        //體力低於30不回傳
+        if (Energy < 30f)
+            return;
+
+        float angel = Mathf.Atan2(vertical, horizontal) / Mathf.PI;
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor)
+        {
+            Energy -= 30f;
+            if (angel >= 0.75f || angel < -0.75f)
+                ani.SetTrigger("dodgeL");
+            else if (angel >= 0.25f)
+                ani.SetTrigger("dodgeF");
+            else if (angel >= -0.25f)
+                ani.SetTrigger("dodgeR");
+            else if (angel >= -0.75f)
+                ani.SetTrigger("dodgeB");
+        }
+        
+        /*
+        //前翻滾
+        if (Input.GetKey(KeyCode.W)
+            && !Input.GetKey(KeyCode.A)
+            && !Input.GetKey(KeyCode.S)
+            && !Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                ani.SetTrigger("dodgeF");
+        }
+        //左翻滾
+        if (!Input.GetKey(KeyCode.W)
+            && Input.GetKey(KeyCode.A)
+            && !Input.GetKey(KeyCode.S)
+            && !Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                ani.SetTrigger("dodgeL");
+        }
+        //後翻滾
+        if (!Input.GetKey(KeyCode.W)
+            && !Input.GetKey(KeyCode.A)
+            && Input.GetKey(KeyCode.S)
+            && !Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                ani.SetTrigger("dodgeB");
+        }
+        //右翻滾
+        if (!Input.GetKey(KeyCode.W)
+            && !Input.GetKey(KeyCode.A)
+            && !Input.GetKey(KeyCode.S)
+            && Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                ani.SetTrigger("dodgeR");
+        }
+        */
     }
 
-    bool onFlood = false;//跳躍判定
+    bool onFloor = false;//跳躍判定
     private void FixedUpdate() //物理刷新
     {
-        onFlood = Physics.Raycast
+        onFloor = Physics.Raycast
             (this.transform.position, new Vector3(0f, -1f, 0f), 1.1f);
+        ani.SetBool("onFloor", onFloor);
     }
+
+    [SerializeField] Image energy = null; 
+    float Energy
+    {
+        get 
+        {
+            return _energy;
+        }
+        set 
+        { 
+            _energy = Mathf.Clamp(value, 0f, maxEnergy); 
+            energy.fillAmount = _energy / maxEnergy;
+        }
+    }
+    float _energy = 100f;
+    float maxEnergy = 100f;
 }
